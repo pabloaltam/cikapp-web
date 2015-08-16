9b2724f6b143cf4082eb8be68dee79cd006af5c9
Pablo Altamirano <pabloaltamver@gmail.com>
2015-08-16 17:01:08 -0300

    Modificación en el index

--- index.php
+++ index.php
@@ -1,34 +1,23 @@
 <?php include 'structure/navbar.php'; ?>
-
 <div align="center">
-     <div class="jumbotron">
-        <div class="container">
-            <h1>Bienvenido</h1>
-            <a class="btn btn-lg btn-round btn-primary" href="#" role="button">Contactános</a>
-        </div>
-    </div>
     <div class="alert alert-warning" role="alert">Estamos trabajando en nuestra nueva plataforma.</div>
- </div>
+    </div>
 <div id="carousel">
-    <!--    
-            IMPORTANT - This carousel can have a special class for a smooth transition "gsdk-transition". Since javascript cannot be overwritten, if you want to use it, you can use the bootstrap.js or bootstrap.min.js from the GSDKit or you can open your bootstrap.js file, search for "emulateTransitionEnd(600)" and change it with "emulateTransitionEnd(1200)"     
-            
-    
     <div id="carousel-example-generic" class="carousel slide gsdk-transition" data-ride="carousel">
       <!-- Indicators
       <ol class="carousel-indicators">
         <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <!-- <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
         <li data-target="#carousel-example-generic" data-slide-to="2"></li>
       </ol>
-
-      <!-- Wrapper for slides 
+ -->
+      <!-- Wrapper for slides -->
       <div class="carousel-inner">
         <div class="item active">
-          <img src="carousel.png" alt="Cikapp Carousel">
+          <img src="structure/img/cover.jpg" alt="Cikapp Carousel">
         </div>
       </div>
-
+        </br>
       <!-- Controls 
       <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
         <span class="fa fa-angle-left"></span>
@@ -36,9 +25,19 @@
       <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
         <span class="fa fa-angle-right"></span>
       </a>
-        -->
+-->
     </div>
 </div> <!-- end carousel -->
+<div align="center">
+     <div class="jumbotron">
+        <div class="container">
+
+            <a class="btn btn-lg btn-round btn-primary" href="#" role="button">Contactános</a>
+        </div>
+    </div>
+
+ </div>
+
 <!--
 <div class="container">
       <div class="jumbotron">
