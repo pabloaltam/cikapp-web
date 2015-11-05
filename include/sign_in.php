a6041f612b4a140b38d7c070382a29984d44c970
gloriaobreque <gloria.obreque@inacapmail.cl>
2015-11-05 01:05:46 -0300

    función recuperar clave
    detalles

--- include/sign_in.php
+++ include/sign_in.php
@@ -78,4 +78,55 @@
 
         session_destroy();
     }
+    
+    function recuperar_clave($email, $rut){
+        require('conexion.php'); //Incluimos la conexion a la base de datos.
+        $sql = "SELECT * FROM usuario WHERE rut=? and email=?";
+        if($stmt = $mysqli->prepare($sql)){
+            $stmt->bind_param('ss',$rut, $email);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if ($rows = $result->fetch_assoc()) {
+                if($rows['codigo'] == 1){ //revisar otra vez
+                    $clave_nueva = clave_aleatoria(10);
+                    $clave_nueva_encriptada = sha1(md5($clave_nueva));
+                    $sql2="UPDATE usuario SET password = '$clave_nueva_encriptada' WHERE rut = '$rut' and email = '$email'";
+                    $stmt->execute();
+                    
+                    $headers = "From: admin@cikapp.com";
+                    $mensaje = '<table width="629" border="0" cellspacing="1" cellpadding="2"> 
+                    <tr> 
+                      <td width="623" align="left"></td> 
+                    </tr> 
+                    <tr> 
+                      <td bgcolor="#2EA354"><div style="color:#FFFFFF; font-size:14; font-family: Arial, Helvetica, sans-serif; text-transform: capitalize; font-weight: bold;"><strong>     Estos son sus datos  '.$rows['nombre'].'</strong></div></td> 
+                    </tr> 
+                    <tr> 
+                      <td height="95" align="left" valign="top"><div style=" color:#000000; font-family:Arial, Helvetica, sans-serif; font-size:12px; margin-bottom:3px;"> RUT: '.$rows['rut'].'</strong><br><br><br> 
+
+                            <strong>SU EMAIL : </strong>'.$rows['email'].'</strong><br><br><br>
+                            <strong>SU NUEVA CONTRASEÑA : </strong>'.$clave_nueva.'</strong><br><br><br> 
+                            <strong>POR FAVOR CAMBIE A UNA NUEVA CONTRASEÑA AL ENTRAR LA PRIMERA VEZ, PARA MAYOR SEGURIDAD.</strong><br><br> 
+                            <strong>GRACIAS POR CONFIAR EN CIKAPP.</strong><br> 
+                      </div> 
+                      </td> 
+                    </tr> 
+                  </table>';
+                    if (!mail("$email", "Nueva contraseña para acceder a su cuenta de cikapp.", "$mensaje", "$headers")) {
+                        echo "<p>No se pudo enviar el email.</p>";
+                    } else {
+                        echo "<p>Se ha enviado un correo con su nueva contraseña su direccion de email, al ingresar con ella a su cuenta por favor cambiela para una mayor seguridad<p>";
+                    }
+                }  
+            }
+        }
+    }
+    
+    function clave_aleatoria($largo) {   //FUNCION PARA CREAR UNA CLAVE ALEATORIA 
+        $pattern = "123456789PIUYTREWQASDFGHJKLMNBVCXZ123456789PLMK1IJNBHUYGVC123456789FTRDXZSEWAQWSDERFTGYHUJ123569876543ERDFREDESWQASWQASDGHGTY";  
+        for($i=0;$i<$largo;$i++) {  
+          $keyal .= $pattern{rand(0,35)};  
+        }  
+        return $keyal;  
+    }  
 ?>
