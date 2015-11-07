da7f1c8dcc4659be50f0ade0f273a5fc658dc9fe
Lackfer® <lackfer@users.noreply.github.com>
2015-11-07 19:22:06 -0300

    Update publicacion.class.php

--- publicacion.class.php
+++ publicacion.class.php
@@ -1,25 +1,25 @@
-	<?php 
-// id,rut,cargo,lugar_trabajo,tipo_contrato,tipo_jornada,fecha_inicio,tipo_publicacion,publicacion,fecha_publicacion
+<?php 
+// id,rut,cargo,lugar_trabajo,tipo_contrato,tipo_jornada,fecha_inicio,publicacion,tipo_publicacion,fecha_publicacion
 	class publicacion
 	{
-
+		
 		function obtienePublicacionesUsuario($rut){
 		include("include/conexion.php");
-		$consulta_publicaciones ="select id,fecha,tipo_publicacion,publicacion,cargo from publicaciones
-		where
-		rut='$rut' order by fecha desc";
+		$rut="11111111-1";
+		$consulta_publicaciones ="SELECT * FROM publicaciones WHERE rut='$rut' ORDER BY fecha_publicacion DESC";
 		$resultado = $mysqli->query($consulta_publicaciones);
 		$i=0;
 		while($fila = $resultado->fetch_assoc()){
-		$arreglo[$i]=array($fila['id'],$fila['fecha_publicacion'],$fila['tipo'],$fila['cargo'],$fila['publicacion']);
+		$arreglo[$i]=array($fila['id'],$fila['cargo'],$fila['lugar_trabajo'],$fila['tipo_contrato'],$fila['tipo_jornada'],$fila['fecha_inicio'],$fila['publicacion'],$fila['tipo_publicacion'],$fila['fecha_publicacion'] );
 		$i++;
 		}
 			return $arreglo;
 		}
-	function agregarPublicacion($rut,$cargo,$lugar_trabajo,$tipo_contrato,$tipo_jornada,$fecha_inicio,$tipo_publicacion,$publicacion){	 
+	function agregarPublicacion($rut,$cargo,$lugar_trabajo,$tipo_contrato,$tipo_jornada,$fecha_inicio,$publicacion,$tipo_publicacion){	 
 		include("include/conexion.php");
-		$agrega_publicacion = "insert into publicaciones(rut,cargo,lugar_trabajo,tipo_contrato,tipo_jornada,fecha_inicio,tipo_publicacion,publicacion)
-		values('$rut','$cargo','$lugar_trabajo','$tipo_contrato','$tipo_jornada','$fecha_inicio','$tipo_publicacion','$publicacion')";
+		$hora= date("Y-m-d H:i:s");    
+		$agrega_publicacion = "insert into publicaciones(rut,cargo,lugar_trabajo,tipo_contrato,tipo_jornada,fecha_inicio,publicacion,tipo_publicacion,fecha_publicacion)
+		values('$rut','$cargo','$lugar_trabajo','$tipo_contrato','$tipo_jornada','$fecha_inicio','$publicacion','$tipo_publicacion','$hora')";
 		$resultado = $mysqli->query($agrega_publicacion);
 		$mysqli->close();
 	}
