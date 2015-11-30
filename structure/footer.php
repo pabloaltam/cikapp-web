b5f2465ecd805431183c25320c434727913654c6
Lackfer® <lackfer@users.noreply.github.com>
2015-11-30 18:05:20 -0300

    Update footer.php

--- structure/footer.php
+++ structure/footer.php
@@ -10,5 +10,11 @@
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
 <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
 <script src="structure/js/light-bootstrap-dashboard.js"></script>
+        <script src="structure/jquery/jquery-1.11.3.min.js"></script>
+        <script src="structure/bstrap/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.12/jquery-ui.min.js" type="text/javascript" charset="utf-8"></script>
+        <script src="structure/js/tag-it.js" type="text/javascript" charset="utf-8"></script>
+        <script src="structure/js/jquery.Rut.min.js"></script>
+        <script src="structure/js/filtro.js"></script>
     </body>
 </html>
