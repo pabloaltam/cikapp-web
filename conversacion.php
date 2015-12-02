b58004b347caa66d9c4a864458e94aa91c871249
Pablo Altamirano <pabloaltamver@gmail.com>
2015-12-02 03:03:05 -0300

    Más modificaciones en mensajes

--- conversacion.php
+++ conversacion.php
@@ -1,13 +1,37 @@
 <?php
 session_start();
-$mi_id = $_SESSION['idUsuario'];  
+$mi_id = $_SESSION['idUsuario']; 
+$usuario = $_POST['user'];
+$idUsuario = $_POST['user'];
+$mensaje = $_POST['message'];
 require('./include/conexion.php');
 
-$query = "SELECT idUsuario, mensaje FROM conversacion order by idConversation asc;";
-$resultado = $mysqli->query($query);
+$obtener_hash = "SELECT hash FROM grupo_mensajes WHERE usuario_uno='$mi_id' AND usuario_dos='$usuario' OR usuario_dos=
+'$mi_id' OR usuario_uno='$usuario' ";
+$resultado = $mysqli->query($obtener_hash);
 
-while ($data = $resultado->fetch_assoc()) {
-	echo "<p><b>".$mi_id ."</b> dice ".$data["mensaje"]."</p>";
-}
+$row = mysqli_fetch_assoc($resultado);
+
+$hash = $row['hash'];
+                           
+
+$mensajes = "SELECT id_remitente, mensaje FROM mensajes WHERE grupo_hash='$hash'";
+$outcome = $mysqli->query($mensajes);
+    while ($listar_mensajes = $outcome->fetch_assoc()) {
+          $id_remitente = $listar_mensajes['id_remitente'];
+          $mensaje = $listar_mensajes['mensaje'];
+          
+          $obtener_usuario = "SELECT `nombre`,`apellido` FROM usuario WHERE idUsuario='$id_remitente' ";
+          $resultado = $mysqli->query($obtener_usuario);
+          while ($rows = $resultado->fetch_assoc()) {
+          $seleccionar_nombre = $rows['nombre'];
+          $seleccionar_apellido = $rows['apellido'];
+      	}
+          $usuario_remitente = $seleccionar_nombre ." " . $seleccionar_apellido;
+          echo "<p><b>$usuario_remitente</b><br/>$mensaje</p>";
+                                        
+  }
+
+
 
 ?>
--- mensajes.php
+++ mensajes.php
@@ -15,10 +15,32 @@
 					<hr>
 				</div>	
 				<div class="row">
-					<form id="formChat" role="form">
+					<form method="post" id="formChat" role="form">
 						<div class="form-group">
 						<?php
-
+						session_start();
+						$mi_id = $_SESSION['idUsuario'];
+						$usuario = $_GET['usuario'];
+						$random_number = rand();
+						
+						if(isset($_POST['message']) && !empty($_POST['message'])) {
+							 include("include/conexion.php");
+							 $mensaje = $_POST['message'];
+							 $revisar_conversacion = "SELECT `hash` FROM `grupo_mensajes` WHERE (`usuario_uno`='$mi_id' AND `usuario_dos`='$usuario') OR (`usuario_uno`='$usuario' AND `usuario_dos`='$mi_id')";
+                             $resultado = $mysqli->query($revisar_conversacion);
+                             $row_cnt = mysqli_num_rows($resultado);
+                             echo "<p>$row_cnt</p>";
+                                                                                  
+                                 if($row_cnt == 1){
+                                                    
+                             } else {
+                             $iniciar_conversacion = "INSERT INTO `grupo_mensajes` VALUES ('$mi_id', '$usuario','$random_number')";
+                             $guardar_mensaje = "INSERT INTO `mensajes` VALUES ('', '$random_number', '$mi_id', '$mensaje')";
+                                                
+                             $resultado = $mysqli->query($iniciar_conversacion);
+                             $resultado = $mysqli->query($guardar_mensaje);                                   
+                             }
+						}
 						$id = $_GET['usuario'];
                         include("include/conexion.php");
         				$consulta_usuarios ="SELECT * FROM usuario WHERE idUsuario = '$id' ";
@@ -33,7 +55,7 @@
 						?>
 
 							<p class="help-block">Enviar mensaje a</p>
-							<input type="hidden" class="form-control" id="user" name="user" value="<?php echo $idUsuario ?>">
+							<input type="text" class="form-control" id="user" name="user" value="<?php echo $idUsuario ?>">
 							<label for="user"><?php echo $nombre ." " .$apellido ." " .$apellidoM; ?></label>
 						</div>
 						<div class="form-group">							
@@ -47,7 +69,7 @@
 						</div>
 						<div class="form-group">				
 							<label for="message">Mensaje</label>
-							<textarea id="message" name="message" placeholder="Enter Message"  class="form-control" rows="3"></textarea>
+							<textarea id="message" name="message" placeholder="Ingresar mensaje"  class="form-control" rows="3"></textarea>
 						</div>
 						<button id="send" class="btn btn-primary" >Enviar</button>						
 					</form>
@@ -59,34 +81,24 @@
 		<script>
 		
 			$(document).on("ready", function(){				
-				registrarMensajes();
 				$.ajaxSetup({"cache":false});
 				setInterval("cargarMensajes()", 500);
 
 			});
 
-			var registrarMensajes = function(){
-				$("#send").on("click", function(e){
-					e.preventDefault();
-					var frm = $("#formChat").serialize();
-					$.ajax({
-						type: "POST",
-						url: "registrar-mensaje.php",
-						data: frm
-					}).done(function(info){
-						console.log( info );
-					})
-				});
-			}
-
 			var cargarMensajes = function(){
+
+				var frm = $("#formChat").serialize();
 				$.ajax({
-					type: "POST",
-					url: "conversacion.php"
+					url: "conversacion.php",
+       				type: "post",
+        			data: frm
 				}).done(function( info ){
 					$("#conversation").html( info );
 					$("#conversation p:last-child").css({"background-color": "lightgreen", 
 						"paddin-bottom": "20px"});
+					$("#conversation").prop("scrollHeight");
+					$("#conversation").scrollTop(altura);
 				})
 			}
 
