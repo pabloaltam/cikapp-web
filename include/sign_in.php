1d5fe7d1b289189a4f0cd8e80f1cd560e4a75497
gloriaobreque <gloria.obreque@inacapmail.cl>
2015-12-07 01:21:38 -0300

    contraseña
    recordar

--- include/sign_in.php
+++ include/sign_in.php
@@ -173,29 +173,20 @@
         if($result = $mysqli->query($sql)){
             if ($rows = $result->fetch_assoc()) {
                 if($rows['codigo'] == 1){ //revisar otra vez
-                    $clave_nueva = clave_aleatoria(5);
+                    $clave_nueva = clave_aleatoria(6);
                     $clave_nueva_encriptada = sha1(md5($clave_nueva));
                     $sql2="UPDATE empresa SET password = '$clave_nueva_encriptada' WHERE rut = '$rut' and emailEmpresa = '$email'";
                     if($res = $mysqli->query($sql2)){
                         //$result->execute();
                         $headers = "From: admin@cikapp.com";
-                        $mensaje = '<table width="629" border="0" cellspacing="1" cellpadding="2"> 
-                        <tr> 
-                          <td width="623" align="left"></td> 
-                        </tr> 
-                        <tr> 
-                          <td bgcolor="#2EA354"><div style="color:#FFFFFF; font-size:14; font-family: Arial, Helvetica, sans-serif; text-transform: capitalize; font-weight: bold;"><strong>     Estos son sus datos  '.$rows['razonSocial'].'</strong></div></td> 
-                        </tr> 
-                        <tr> 
-                          <td height="95" align="left" valign="top"><div style=" color:#000000; font-family:Arial, Helvetica, sans-serif; font-size:12px; margin-bottom:3px;"> RUT: '.$rows['rut'].'</strong><br><br><br> 
-                                <strong>SU EMAIL : </strong>'.$rows['emailEmpresa'].'</strong><br><br><br>
-                                <strong>SU NUEVA CONTRASEÑA : </strong>'.$clave_nueva.'</strong><br><br><br> 
-                                <strong>POR FAVOR CAMBIE A UNA NUEVA CONTRASEÑA AL ENTRAR LA PRIMERA VEZ, PARA MAYOR SEGURIDAD.</strong><br><br> 
-                                <strong>GRACIAS POR CONFIAR EN CIKAPP.</strong><br> 
-                          </div> 
-                          </td> 
-                        </tr> 
-                        </table>';
+                        $mensaje = '
+                        Estos son sus datos  '.$rows['razonSocial'].' - 
+                        RUT: '.$rows['rut'].' - 
+                        SU EMAIL : '.$rows['emailEmpresa'].' - 
+                        SU NUEVA CONTRASEÑA : '.$clave_nueva.' - 
+                        POR FAVOR CAMBIE A UNA NUEVA CONTRASEÑA AL ENTRAR LA PRIMERA VEZ, PARA MAYOR SEGURIDAD.
+                        GRACIAS POR CONFIAR EN CIKAPP.
+                        ';
                         if (!mail("$email", "Nueva contraseña para acceder a su cuenta de cikapp.", "$mensaje", "$headers")) {
                             echo "<p>No se pudo enviar el email.</p>";
                         } else {
@@ -218,29 +209,20 @@
         if($result = $mysqli->query($sql)){
             if ($rows = $result->fetch_assoc()) {
                 if($rows['codigo'] == 1){ //revisar otra vez
-                    $clave_nueva = clave_aleatoria(5);
+                    $clave_nueva = clave_aleatoria(6);
                     $clave_nueva_encriptada = sha1(md5($clave_nueva));
                     $sql2="UPDATE usuario SET password = '$clave_nueva_encriptada' WHERE rut = '$rut' and email = '$email'";
                     if($res = $mysqli->query($sql2)){
                         //$result->execute();
                         $headers = "From: admin@cikapp.com";
-                        $mensaje = '<table width="629" border="0" cellspacing="1" cellpadding="2"> 
-                        <tr> 
-                          <td width="623" align="left"></td> 
-                        </tr> 
-                        <tr> 
-                          <td bgcolor="#2EA354"><div style="color:#FFFFFF; font-size:14; font-family: Arial, Helvetica, sans-serif; text-transform: capitalize; font-weight: bold;"><strong>     Estos son sus datos  '.$rows['nombre'].'</strong></div></td> 
-                        </tr> 
-                        <tr> 
-                          <td height="95" align="left" valign="top"><div style=" color:#000000; font-family:Arial, Helvetica, sans-serif; font-size:12px; margin-bottom:3px;"> RUT: '.$rows['rut'].'</strong><br><br><br> 
-                                <strong>SU EMAIL : </strong>'.$rows['email'].'</strong><br><br><br>
-                                <strong>SU NUEVA CONTRASEÑA : </strong>'.$clave_nueva.'</strong><br><br><br> 
-                                <strong>POR FAVOR CAMBIE A UNA NUEVA CONTRASEÑA AL ENTRAR LA PRIMERA VEZ, PARA MAYOR SEGURIDAD.</strong><br><br> 
-                                <strong>GRACIAS POR CONFIAR EN CIKAPP.</strong><br> 
-                          </div> 
-                          </td> 
-                        </tr> 
-                        </table>';
+                        $mensaje = '
+                        Estos son sus datos  '.$rows['nombre'].' - 
+                        RUT: '.$rows['rut'].' - 
+                        SU EMAIL : '.$rows['email'].' - 
+                        SU NUEVA CONTRASEÑA : '.$clave_nueva.' - 
+                        POR FAVOR CAMBIE A UNA NUEVA CONTRASEÑA AL ENTRAR LA PRIMERA VEZ, PARA MAYOR SEGURIDAD. 
+                        GRACIAS POR CONFIAR EN CIKAPP.
+                          ';
                         if (!mail("$email", "Nueva contraseña para acceder a su cuenta de cikapp.", "$mensaje", "$headers")) {
                             echo "<p>No se pudo enviar el email.</p>";
                         } else {
@@ -259,11 +241,15 @@
     }
     
     function clave_aleatoria($largo) {   //FUNCION PARA CREAR UNA CLAVE ALEATORIA 
-        $pattern = "123456789PIUYTREWQASDFGHJKLMNBVCXZ123456789PLMK1IJNBHUYGVC123456789FTRDXZSEWAQWSDERFTGYHUJ123569876543ERDFREDESWQASWQASDGHGTY";  
-        for($i=0;$i<$largo;$i++) {  
-          $keyal = $pattern{rand(0,35)};  
-        }  
-        return $keyal;  
+        $cadena = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+        $longitudCadena = strlen($cadena);
+        $pass = "";
+        $longitudPass = $largo;
+        for($i=1 ; $i<=$longitudPass ; $i++){
+            $pos=rand(0,$longitudCadena-1);
+            $pass .= substr($cadena,$pos,1);
+        }
+        return $pass; 
     }
     function esEmpresa($rut){
         $res = substr($rut,0,2);
